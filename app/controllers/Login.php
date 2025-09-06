55ba66b2cee07772896900922307164f53f72656
Shin Welivitiya <shinivitiya@gmail.com>
2025-09-06 13:44:51 +0530

    feat(ui): Add events widget to home page

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -49,17 +49,4 @@ class Login extends Controller
             'errors' => $data['errors'],
         ]);
     }
-
-    public function logout()
-    {
-        // Optional: clear and destroy session safely
-        $_SESSION = [];
-        if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
-            setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-        }
-        session_destroy();
-        header("Location: /login");
-        exit;
-    }
 }
