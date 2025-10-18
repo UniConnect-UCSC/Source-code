b34b65428689ac0294be07102df676a00b864669
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2025-10-18 14:00:41 +0530

    Added Ajax utility class it to be loaded on each page by default

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -29,6 +29,7 @@
 <body>
     <?= $content ?>
 
+    <script src="/assets/js/ajax.js"></script>
     <script src="/assets/js/navbar.js"></script>
     <script src="/assets/js/postOptions.js"></script>
 
