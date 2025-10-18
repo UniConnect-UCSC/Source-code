b78ab9ed2b571a08b85ab781e3c47656e4e5e2fa
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2025-10-18 14:00:52 +0530

    Add InfinityScroll class for dynamic content loading

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -30,6 +30,7 @@
     <?= $content ?>
 
     <script src="/assets/js/ajax.js"></script>
+    <script src="/assets/js/infinityScrolling.js"></script>
     <script src="/assets/js/navbar.js"></script>
     <script src="/assets/js/postOptions.js"></script>
 
---
+++ public/assets/js/infinityScrolling.js
@@ -0,0 +1,46 @@
+/** Constructor for InfinityScroll class
+ * @param {string} scrollIdentifier (This is the ID that get's passed to the server for requesting which data is required)
+ * @param {string} fetchUrl (URL to fetch data from)
+ * @param {HTMLElement} parentElement (Element to append the fetched data to)
+ * @param {(data: any) => HTMLElement} renderer (Function that takes data and returns an HTMLElement[card] to be appended)
+ * @param {number} [offset] (Starting offset for data fetching defaults to 0)
+ * @param {number} [limit] (Number of items to fetch per request defaults to 10)
+*/
+
+
+class InfinityScroll{
+    
+
+    constructor(scrollIdentifier, fetchUrl, parentElement, renderer, offset = 0, limit = 10){
+        if (!window.Ajax) throw new Error('InfinityScroll requires Ajax class to be loaded.');
+        this.scrollIdentifier = scrollIdentifier;
+        this.fetchUrl = fetchUrl;
+        this.parentElement = parentElement;
+        this.renderer = renderer;
+        this.offset = offset;
+        this.limit = limit;
+    }
+
+    async loadNextElements(){
+        const data = {
+            scrollIdentifier: this.scrollIdentifier,
+            offset: this.offset,
+            limit: this.limit
+        }
+
+        try {
+            const response = await Ajax.post(this.fetchUrl, data);
+
+            response.forEach(item => {
+                const element = this.renderer(item);
+                this.parentElement.appendChild(element);
+            });
+
+            this.offset += this.limit;
+
+        } catch(error){
+            console.error('InfinityScroll.loadNextElements: Error fetching data', error);
+        }
+
+    }
+}
