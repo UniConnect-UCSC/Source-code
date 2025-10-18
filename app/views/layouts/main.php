58bd29d6db57164b70d264e4512a90a4978be8ac
Ranidu-Premathilaka <114408219+Ranidu-Premathilaka@users.noreply.github.com>
2025-10-18 14:57:56 +0530

    Feature/ajax support (#20)
    * Added Ajax utility class it to be loaded on each page by default
    
    * Add InfinityScroll class for dynamic content loading
    
    * change header temp variable name to prevent conflicts
    
    * Change name to infinityScroll
    
    * Fixed variable header in request method to prevent conflicts

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -29,6 +29,8 @@
 <body>
     <?= $content ?>
 
+    <script src="/assets/js/ajax.js"></script>
+    <script src="/assets/js/infinityScroll.js"></script>
     <script src="/assets/js/navbar.js"></script>
     <script src="/assets/js/postOptions.js"></script>
 
