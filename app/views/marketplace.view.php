5dc4161c60f4b7b673f0b38e95fa9ea046813d2a
Shin Welivitiya <131185110+Shinvitiya@users.noreply.github.com>
2025-10-23 13:13:19 +0530

    Feature/marketplace (#37)
    * Marketplace CRUD done and add some UI
    
    * feat(chore) Setup base for merging with dev
    
    * Split marketplace into components
    
    * Dev (#32)
    
    * Feature/marketplace (#28)
    
    * Marketplace CRUD done and add some UI
    
    * feat(chore) Setup base for merging with dev
    
    * Split marketplace into components
    
    ---------
    
    Co-authored-by: Sandaru Ruwaneka <sandaruruwaneka21@gmail.com>
    
    * Feature/ajax support (#30)
    
    * Added Ajax utility class it to be loaded on each page by default
    
    * Add InfinityScroll class for dynamic content loading
    
    * change header temp variable name to prevent conflicts
    
    * Change name to infinityScroll
    
    * Fixed variable header in request method to prevent conflicts
    
    * moved ajax and infinityscroll to the head for References issues
    
    * Removed body ajax and infinity scroll scripts
    
    * bugfix(js/infinityScroll) return false once responses are empty
    
    * feat(js/infinityscroll): Only one loadNextElements is allowed to be called at a time
    feat(js/infinityscroll): resetScroll method creation
    feat(js/infinityscroll): refresh method for refreshing all the elements that's fetched before
    
    * bugfix(core/model) Changed the order of ORDER BY clause with LIMIT and OFFSET (#29)
    
    * Feature/profile (#31)
    
    * feat(chore) Setup update for post
    
    * feat(fix) Fixed update issue
    
    ---------
    
    Co-authored-by: Sandaru Ruwaneka <sandaruruwaneka21@gmail.com>
    Co-authored-by: Ranidu-Premathilaka <114408219+Ranidu-Premathilaka@users.noreply.github.com>
    
    * feat(ui) Setup marketplace page
    
    ---------
    
    Co-authored-by: Sandaru Ruwaneka <sandaruruwaneka21@gmail.com>
    Co-authored-by: Ranidu-Premathilaka <114408219+Ranidu-Premathilaka@users.noreply.github.com>

--- app/views/marketplace.view.php
+++ app/views/marketplace.view.php
@@ -1,3 +1,14 @@
+<?php
+require_once(__DIR__ . "/../models/MarketplaceItem.php");
+require_once(__DIR__ . "/../models/MarketplaceCategories.php");
+
+$itemsModels = new MarketplaceItem();
+$items = $itemsModels->where([], null, null, ['created_at' => 'DESC']);
+
+$categoryModel = new MarketplaceCategories();
+$categories = $categoryModel->where([]);
+?>
+
 <?php component("navbar"); ?>
 
 <div class="marketplace-layout">
@@ -6,7 +17,7 @@
         <div class="market-items-header">
             <h2>Marketplace</h2>
             <div class="market-items-header-buttons">
-                <a href="/marketplace/sellItem" class="btn-sellItem">Sell an Item</a>
+                <div class="btn-sellItem" onclick="openSellItemModal()">Sell an Item</div>
                 <a href="/marketplace/myItems" class="btn-myItems">My Items</a>
             </div>
         </div>
@@ -16,15 +27,80 @@
                 foreach ($items as $item) {
                     component("marketplaceItem", [
                         "id" => $item->id,
+                        "studentID" => $item->student_id,
                         "title" => $item->title,
+                        "description"   => $item->description,
                         "price" => $item->price,
+                        "categoryID" => $item->category_id,
                         "status" => $item->status,
-                        //              "image" => $item->image,
                         "created_at" => $item->created_at,
                     ]);
                 }
             }
             ?>
+        </div>
+    </div>
+</div>
+
+
+<div class="sell-item-modal" id="sell-item-modal" onclick="closeSellItemModal()">
+    <div class="sell-item-modal-content" onclick="event.stopPropagation()">
+        <div class="sell-item-modal-header">
+            <h2>Sell an Item</h2>
+        </div>
+        <form id="sell-item-form" enctype="multipart/form-data" autocomplete="off">
+            <div class="sell-item-modal-post-content">
+
+                <div>
+                    <input type="text" name="title" id="item-title" placeholder="Item Title">
+                    <div class="error-message" id="sell-item-title-error"></div>
+                </div>
+
+                <div>
+                    <textarea name="description" id="item-description" placeholder="Description"></textarea>
+                    <div class="error-message" id="sell-item-description-error"></div>
+                </div>
+
+                <div>
+                    <input type="number" name="price" id="item-price" placeholder="Price" min="0">
+                    <div class="error-message" id="sell-item-price-error"></div>
+                </div>
+
+                <div>
+                    <select name="category_id" id="item-category">
+                        <option value="">Select Category</option>
+                        <?php
+                        foreach ($categories as $cat) {
+                            echo '<option value="' . htmlspecialchars($cat->id) . '">' . htmlspecialchars($cat->name) . '</option>';
+                        }
+                        ?>
+                    </select>
+
+                    <div class="error-message" id="sell-item-category-error"></div>
+                </div>
+
+                <div class="sell-item-media-input">
+                    <input id="sell-item-media-input" name="media" type="file" accept="image/*" />
+                </div>
+
+                <div class="sell-item-image-preview">
+                    <img id="sell-item-preview-img" src="">
+                </div>
+
+                <div onclick="openSellItemImageSelector()" class="sell-item-image-button">
+                    <i data-lucide="image"></i>
+                    Photo
+                </div>
+                <div class="error-message" id="sell-item-media-error"></div>
+
+
+            </div>
+            <div class="sell-item-modal-actions">
+                <button type="submit" class="sell-item-modal-button">Sell Item</button>
+            </div>
+        </form>
+        <div class="loading-spinner" id="sell-item-loading-spinner" style="display:none;">
+            <div class="spinner"></div>
         </div>
     </div>
 </div>
--- app/views/myItems.view.php
+++ app/views/myItems.view.php
@@ -1,29 +1,39 @@
+<?php
+require_once(__DIR__ . "/../models/MarketplaceItem.php");
+require_once(__DIR__ . "/../models/MarketplaceCategories.php");
+
+$itemsModel = new MarketplaceItem();
+$items = $itemsModel->where([['student_id', '=', $_SESSION['user_id']]], null, null, ['created_at' => 'DESC']);
+
+$categoryModel = new MarketplaceCategories();
+$categories = $categoryModel->where([]);
+?>
+
 <?php component("navbar"); ?>
-<div class="home-layout">
+<div class="marketplace-layout">
     <?php component("navPanel"); ?>
-        <div class="feed">
-            <div class="feed-header">
-                <h2>My Items</h2>
-            </div>
-            <div class ="feed-items">
-             <?php
+    <div class="my-items-container">
+
+        <div class="">
+            <h2>My Items</h2>
+        </div>
+
+        <div class="my-items-list">
+            <?php
             if (!empty($items)) {
-             foreach ($items as $item) {
-             component("marketplaceItem", [
-                "id" => $item->id,
-                "title" => $item->title,
-                "price" => $item->price,
-                "status" => $item->status,
-  //              "image" => $item->image,
-                "created_at" => $item->created_at,
-                "editUrl" => "/marketplace/editItem/{$item->id}",
-                "deleteUrl" => "/marketplace/deleteItem/{$item->id}",
-             ]); 
-             }
+                foreach ($items as $item) {
+                    component("marketplaceItem", [
+                        "id" => $item->id,
+                        "title" => $item->title,
+                        "price" => $item->price,
+                        "status" => $item->status,
+                        "created_at" => $item->created_at,
+                        "myItems" => true
+                    ]);
+                }
             } else {
                 echo "<p>You have not listed any items yet.</p>";
-            }?>
+            } ?>
         </div>
     </div>
-    <?php component("widgetPanel"); ?>
 </div>
