04e834326de45a6ced0e7e66c3b18b0fe0c74465
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2025-12-25 09:29:38 +0530

    backend(notifications) Implemented notification handling at the backend

---
+++ app/controllers/Notifications.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once __DIR__ . '/../core/functions.php';
+require_once __DIR__ . '/../models/InAppUserNotifications.php';
+
+class Notifications extends Controller
+{
+    private function getNextNotifications($limit, $offset) {
+        $notificationModel = new NotificationModel();
+        $data = $notificationModel->getNextNotifications($limit, $offset);
+        
+        // Handled on client side
+        //$data->metadata = json_decode($data->metadata, true);
+
+        return $data;
+
+    }
+
+    private function markAllNotificationAsRead($userId) {
+        $notificationModel = new NotificationModel();
+        return $notificationModel->markAllAsRead($userId);
+    }
+
+    private function markNotificationAsRead($notificationId) {
+        $notificationModel = new NotificationModel();
+        return $notificationModel->markAsRead($notificationId);
+    }
+
+    public function scrollable() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+            $data = parseRequestData();
+
+            header('Content-Type: application/json');
+
+            switch($data['scrollIdentifier']) {
+                case 'getNotifications':
+                    $response = $this->getNextNotifications($data['limit'], $data['offset']);
+                    echo json_encode($response);
+                    break;
+                default:
+                    echo json_encode(['error' => 'Invalid action']);
+                    break;
+            }
+        }
+    }
+
+    // Return values wouldn't be received due to the fire and forget nature of the request
+    public function markAsRead() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+            $data = parseRequestData();
+            header('Content-Type: application/json');
+
+            $notificationId = $data ?? null;
+
+             if($notificationId && $this->markNotificationAsRead($notificationId)) {
+                 echo json_encode(['success' => true, 'message' => "Notification $notificationId marked as read."]);
+             } else {
+                 echo json_encode(['error' => 'Notification ID is required']);
+             }
+        }
+    }
+
+    public function markAllAsRead() {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+        
+            header('Content-Type: application/json');
+
+             if($this->markAllNotificationAsRead($_SESSION['user_id'])) {
+                 echo json_encode(['success' => true, 'message' => "All notification marked as read."]);
+             } else {
+                 echo json_encode(['error' => 'Notification ID is required']);
+             }
+        }
+    }
+}
---
+++ app/models/InAppUserNotifications.php
@@ -0,0 +1,66 @@
+<?php
+
+class NotificationModel {
+    use Model;
+    protected $table = 'in_app_user_notifications';
+
+    public function getNextNotifications($limit, $offset) {
+
+        $selected = [
+            "m.*",
+            "n.type",
+            "n.title",
+            "n.message",
+            "n.metadata",
+        ];
+
+        $join = [
+            [
+                "in_app_notifications",
+                "m.notification_id = n.id",
+                "INNER",
+                "n"
+            ]
+        ];
+
+        error_log("Fetching notifications for user_id: " . $_SESSION['user_id'] . " with limit: $limit and offset: $offset");
+        return $this->where(
+            conditions: [['m.user_id', '=', $_SESSION['user_id']]],
+            limit: $limit,
+            offset: $offset,
+            orderBy: ['created_at' => 'DESC'],
+            join: $join,
+            selected: $selected
+        );
+    }
+
+    public function markAsRead($notificationId) {
+        $this->update(
+            id: $notificationId,
+            data: [
+                'is_read' => 1,
+                'read_at' => date('Y-m-d H:i:s', time())
+            ],
+            id_column: 'id'
+        );
+
+        return true;
+    }
+
+    // ISSUE: Time zone issue can really affect the read_at timestamp if user is in different timezone
+    // Migate to NOW() in SQL
+
+    public function markAllAsRead($userId) {
+        $this->update(
+            id: $userId,
+            data: [
+                'is_read' => 1,
+                'read_at' => date('Y-m-d H:i:s', time())
+            ],
+            id_column: 'user_id'
+        );
+
+        // update successful-ness isn't being passed by the database/query method
+        return true;
+    }
+}
