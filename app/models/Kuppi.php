4d1ec01e10c4e71b1c7e47949ae9dbd39966b8a8
Sandaru Ruwaneka <sandaruruwaneka21@gmail.com>
2026-04-04 13:37:24 +0530

    Fixed a bug in a favorite button

--- app/models/Kuppi.php
+++ app/models/Kuppi.php
@@ -13,7 +13,8 @@ class KuppiModel {
             ["users", "m.host_id = h.id", "INNER", "h"],
             ["users", "m.requester_id = r.id", "LEFT", "r"],
             ["kuppi_categories","m.category_id = c.id", "INNER" ,"c"],
-            ["kuppi_participants",["m.id = p.kuppi_id",["p.user_id", "=", $_SESSION["user_id"]]],"LEFT","p"]
+            ["kuppi_participants",["m.id = p.kuppi_id",["p.user_id", "=", $_SESSION["user_id"]]],"LEFT","p"],
+            ["kuppi_favorites",["m.id = f.kuppi_id",["f.user_id", "=", $_SESSION["user_id"]]],"LEFT","f"]
         ];
         $orderBy = [
             "m.kuppi_date_time" =>'DESC'
@@ -26,7 +27,8 @@ class KuppiModel {
             ["h.l_name", "host_l_name"],
             ["r.f_name", "requester_f_name"],
             ["r.l_name", "requester_l_name"],
-            ["CASE WHEN p.kuppi_id IS NULL THEN 0 ELSE 1 END", "is_participating"]
+            ["CASE WHEN p.kuppi_id IS NULL THEN 0 ELSE 1 END", "is_participating"],
+            ["CASE WHEN f.kuppi_id IS NULL THEN 0 ELSE 1 END", "is_favorite"]
         ];
 
 
@@ -133,7 +135,8 @@ class KuppiModel {
             ["h.l_name", "host_l_name"],
             ["r.f_name", "requester_f_name"],
             ["r.l_name", "requester_l_name"],
-            ["c.category_name", "category"]
+            ["c.category_name", "category"],
+            ["CASE WHEN f.kuppi_id IS NULL THEN 0 ELSE 1 END", "is_favorite"]
         ];
 
         $data = $this->where(
