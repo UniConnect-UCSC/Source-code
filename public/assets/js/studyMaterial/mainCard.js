0efdfa6fdb2db6d6895253a70d16ca1f9a69afb1
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2026-04-15 21:22:59 +0530

    feat(sm-filtering) implemented search and sort by functionality

--- public/assets/js/studyMaterial/mainCard.js
+++ public/assets/js/studyMaterial/mainCard.js
@@ -1,13 +1,48 @@
 (function() {
     const smGrid = document.getElementById('sm-grid');
+    const orderSelectElement = document.getElementById('sm-sort-select');
+    const searchInputElement = document.getElementById('sm-search-input');
+
+    const debounceDelay = 300;
+    var debounceTimer = null;
+
+    var selectedOrderBy = 'recent';
+    var searchTerm = '';
 
     mainStudyMaterialScroll.setupAutoLoadOnScroll();
     mainStudyMaterialScroll.loadNextElements();
 
-    // mainStudyMaterialScroll.setContextProvider(() => {
-    //     return {
-    //     };
-    // });
+    mainStudyMaterialScroll.setContextProvider(() => {
+        return {
+            orderBy: selectedOrderBy,
+            searchTerm: searchTerm
+        };
+    });
+
+    orderSelectElement.addEventListener('change', (e) => {
+        selectedOrderBy = e.target.value;
+        mainStudyMaterialScroll.refresh();
+    });
+
+    searchInputElement.addEventListener('input', (e) => {
+       
+        if(debounceTimer) {
+            clearTimeout(debounceTimer);
+        }
+
+        debounceTimer = setTimeout(() => {
+            searchTerm = e.target.value;
+            mainStudyMaterialScroll.refresh();
+        }, debounceDelay);
+    });
+
+    searchInputElement.addEventListener('keypress', (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            searchTerm = e.target.value;
+            mainStudyMaterialScroll.refresh();
+        }
+    });
 
     smGrid.addEventListener('sm:view-click', (e) => {
         const { id } = e.detail;
