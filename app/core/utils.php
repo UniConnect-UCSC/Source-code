40990d622fba4267d719d45a352c990c4832b79c
Shin Welivitiya <shinivitiya@gmail.com>
2026-04-19 15:27:44 +0530

    Merged dev into feature/admin and resolved utils.php conflict

--- app/core/utils.php
+++ app/core/utils.php
@@ -40,9 +40,57 @@ $navbarPanelLinks = [
         "pageLink" => "/friends",
         "icon" => "handshake"
     ],
-    // [
-    //     "pageName" => "Groups",
-    //     "pageLink" => "/",
-    //     "icon" => "users"
-    // ],
+    [
+        "pageName" => "University Representative",
+        "pageLink" => "/unirepresentative",
+        "icon" => "shield"
+    ]
+];
+
+$adminPanelLinks = [
+    [
+        "pageName" => "Dashboard",
+        "pageLink" => "/admin/dashboard",
+        "icon" => "layout-dashboard"
+    ],
+    [
+        "pageName" => "Users",
+        "pageLink" => "/admin/users",
+        "icon" => "users"
+    ],
+    [
+        "pageName" => "Posts",
+        "pageLink" => "/admin/posts",
+        "icon" => "book-image"
+    ],
+    [
+        "pageName" => "Events",
+        "pageLink" => "/admin/events",
+        "icon" => "calendar"
+    ],
+    [
+        "pageName" => "Uni Representative",
+        "pageLink" => "/admin/unirep",
+        "icon" => "calendar"
+    ],
+    [
+        "pageName" => "Kuppi",
+        "pageLink" => "/admin/kuppi ",
+        "icon" => "graduation-cap"
+    ],
+    [
+        "pageName" => "Study Materials",
+        "pageLink" => "/admin/studymaterials",
+        "icon" => "notebook-pen"
+    ],
+    [
+        "pageName" => "Boardings",
+        "pageLink" => "/admin/boardings",
+        "icon" => "bed"
+    ],
+    [
+        "pageName" => "Marketplace",
+        "pageLink" => "/admin/marketplace",
+        "icon" => "store"
+    ],
 ];
