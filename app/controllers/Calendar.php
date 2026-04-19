0367cecb3cca6ab7d7c831b2cd42975f2329084a
Ranidu-Premathilaka <114408219+Ranidu-Premathilaka@users.noreply.github.com>
2026-04-19 15:30:54 +0530

    Feature/calender (#197)
    * midway commit
    
    * feat(calender) "calender" fully implemented UI and backend

--- app/controllers/Calendar.php
+++ app/controllers/Calendar.php
@@ -1,11 +1,39 @@
 <?php 
 class calendar extends Controller
 {
+
+    private function getKuppiFavoritesAndParticipants()
+    {
+        $userId = $_SESSION['user_id'];
+
+        require_once __DIR__ . '/../models/Kuppi.php';
+        $kuppiModel = new KuppiModel();
+        return $kuppiModel->getKuppiFavoritesAndParticipants($userId);
+
+    }
+
+    private function getEventsFavoritesAndParticipants()
+    {
+        require_once __DIR__ . '/../models/Event.php';        
+
+        $userId = $_SESSION['user_id'];
+        $eventModel = new EventModel();
+        return $eventModel->getEventFavoritesAndParticipants($userId);
+    }
+
+    public function getData(){
+        $data = [];
+
+        $data['events'] = $this->getEventsFavoritesAndParticipants();
+        $data['kuppi'] = $this->getKuppiFavoritesAndParticipants();
+
+        header('Content-Type: application/json');
+        echo json_encode($data);
+        return;
+    }
+
     public function index()
     {
-        // Example bookmarked dates
-        $bookmarkedDates = ['2024-10-15', '2024-10-20']; // Format: ['YYYY-MM-DD', 'YYYY-MM-DD', ...]
-
         $this->view('calendar', [
             'title' => 'Calendar • UniConnect',
             'head' => '
@@ -17,7 +45,6 @@ class calendar extends Controller
             <link rel="stylesheet" href="/assets/css/components/eventsWidget.css">
             <link rel="stylesheet" href="/assets/css/components/calender.css">
             ',
-            'bookmarkedDates' => $bookmarkedDates,
         ]);
     }
 }
