9b0ba2cec628e6237aa342cd773973b7b354647f
Ranidu-Premathilaka <114408219+Ranidu-Premathilaka@users.noreply.github.com>
2026-04-20 08:15:25 +0530

    bugfix(kuppi-report) : added m star (#201)
    Co-authored-by: Sandaru Ruwaneka <sandaruruwaneka21@gmail.com>

--- app/models/kuppiReport.php
+++ app/models/kuppiReport.php
@@ -24,6 +24,7 @@ class KuppiReportModel {
         ];
 
         $selected = [
+            "m.*"
             "k.*",
             ["h.f_name" , "host_f_name"],
             ["r.f_name" , "requester_f_name"],
