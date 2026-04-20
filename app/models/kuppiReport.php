109a2465084213edf5ca98f79e24f73312c5573e
Ranidu-Premathilaka <ranidulakshan001@gmail.com>
2026-04-20 08:20:21 +0530

    critical-bugfix(kuppi) The previous PR caused an unstable state in this please don't use :)

--- app/models/kuppiReport.php
+++ app/models/kuppiReport.php
@@ -24,7 +24,7 @@ class KuppiReportModel {
         ];
 
         $selected = [
-            "m.*"
+            "m.*",
             "k.*",
             ["h.f_name" , "host_f_name"],
             ["r.f_name" , "requester_f_name"],
